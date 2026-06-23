096eb122b083bcafc76a7deaf5dd29b3eebb633b
sahilyadav-15 <s6906433@gmail.com>
2026-06-23 13:00:16 +0530

    dd

--- api/server.js
+++ api/server.js
@@ -1,5 +1,21 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 // app config
 const app = express();
+const port = process.env.PORT || 3577;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.listen(port, () => {
+  console.log("Server running on", port);
+});
