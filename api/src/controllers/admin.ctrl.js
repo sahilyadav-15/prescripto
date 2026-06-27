df9edfefcb67669ad8ba538aae9cedbb2e223fc3
sahilyadav-15 <sahilyadav.cs01@gmail.com>
2026-06-27 09:50:31 +0530

    feat: add admin login functionality and authentication middleware

--- api/src/controllers/admin.ctrl.js
+++ api/src/controllers/admin.ctrl.js
@@ -1,6 +1,7 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
+import jwt from "jsonwebtoken";
 import doctorModel from "../models/doctor.model.js";
 
 export const addDoctor = async (req, res) => {
@@ -104,6 +105,26 @@ export const addDoctor = async (req, res) => {
 
     await newDoctor.save();
     res.json({ success: true, message: "Doctor added" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message || "Error" });
+  }
+};
+
+// API for admin login
+export const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.status(200).json({ success: true, token });
+    } else {
+      res.status(400).json({ success: false, message: "Invalid credentials" });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error.message || "Error" });
--- api/src/routes/admin.route.js
+++ api/src/routes/admin.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { addDoctor } from "../controllers/admin.ctrl.js";
+import { addDoctor, loginAdmin } from "../controllers/admin.ctrl.js";
 import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/auth.admin.mw.js";
 
 const adminRoute = Router();
 
-adminRoute.post("/add-doctor", upload.single("image"), addDoctor);
+adminRoute.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
+adminRoute.post("/login", loginAdmin);
 
 export default adminRoute;
